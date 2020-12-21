e18cc9a4de781f98a333308ee8f260724649219e
pafmon <pafmon@gmail.com>
2020-12-21 21:32:07 +0100

    log added

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     Last message: <pre>{{ message }}</pre>
     Cache: <pre>{{ cache }}</pre>
+    Log: <span>{{ log }}</span>
     <input v-model="message">
     <button v-on:click="newMessage">new Message</button>
   </div>
@@ -13,13 +14,16 @@ export default {
     data() {
         return {
             message: 'n/a',
-            cache: {}
+            cache: {},
+            log : "Init"
         };
     },
     methods: {
         newMessage: function () {
              
-            
+            this.log = "Clicked New Message";
+            this.$forceUpdate(); 
+
             function randomString(length) {
                 return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
             }
@@ -27,13 +31,20 @@ export default {
             var newCache = {};
             var view = this;
 
+            this.log = "Looking for cache...";
+            this.$forceUpdate(); 
 
             fetch("http://cache.governify.io/sprscrt/rs/vtc")
             .then(function (response) { 
+                view.log = "Response from cache received";
+                view.$forceUpdate(); 
+                
                 return response.json();    
             })
             .then(function (remoteCache) {
-                
+                view.log = "Response from cache parsed";
+                view.$forceUpdate(); 
+
                 if(remoteCache){
                     newCache = remoteCache;
                 }
@@ -44,8 +55,13 @@ export default {
                 
                 var values = (new URLSearchParams(newCache)).toString();
 
+                view.log = "new cache built";
+                view.$forceUpdate(); 
                 fetch("http://cache.governify.io/sprscrt/rs/store/vtc?"+values)
                 .then(function () { 
+                    view.log = "Cache updated";
+                    view.$forceUpdate(); 
+
                     view.cache = newCache;
                     view.$forceUpdate(); 
                 });
